2e435d2dac0c5a6aeaae64ad13fa7a5f30ba5460
nhat910 <nhat.hoang@diqit.io>
2022-09-22 17:11:27 +0700

    - Update lib

--- BaseProjectKotlin/app/src/main/java/com/nhat0910/basekotlin/MainActivity.kt
+++ BaseProjectKotlin/app/src/main/java/com/nhat0910/basekotlin/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.nhat0910.basekotlin
 
-import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
